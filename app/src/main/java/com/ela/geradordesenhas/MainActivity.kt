e832232864269c2b42a1c69a5c4a13a7850ea940
Ale TI <alessandrodev@outlook.com>
2023-07-03 12:27:20 -0300

    final commit

--- app/src/main/java/com/ela/geradordesenhas/MainActivity.kt
+++ app/src/main/java/com/ela/geradordesenhas/MainActivity.kt
@@ -2,10 +2,35 @@ package com.ela.geradordesenhas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ela.geradordesenhas.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val alfabeto = arrayOf(
+        "A","B","C","D","E","F","G","H",
+        "I","J","K","L","M","N","O","P","Q","R",
+        "S","T","U","V", "W","X","Y","Z")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btGerarSenha.setOnClickListener{
+            gerarSenhaAleatoria()
+        }
+    }
+    private fun gerarSenhaAleatoria(){
+        val primeiraLetra=(alfabeto.indices).random()
+        val segundaLetra=(alfabeto.indices).random()
+
+        val letraAleatoria1 = alfabeto[primeiraLetra]
+        val letraAleatoria2 = alfabeto[segundaLetra]
+
+        val numerosAleatorios = Random
+        val numeros = numerosAleatorios.nextInt(9999)
+
+        binding.txtSenha.text = "$letraAleatoria1$letraAleatoria2-$numeros"
     }
 }
